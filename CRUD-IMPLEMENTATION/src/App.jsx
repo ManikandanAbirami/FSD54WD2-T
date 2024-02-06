a17d8f2cfe382b24bab61311aff1d61e0ef2c5cf
Manikandan Anbalagan <manikandana121290@gmail.com>
2024-02-06 20:41:55 +0530

    CRUD

--- CRUD-IMPLEMENTATION/src/App.jsx
+++ CRUD-IMPLEMENTATION/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import axios from "axios";
-import AppForm from './AppForm';
+import PostForm from './PostForm';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -9,24 +9,33 @@ function App() {
 
   const getPosts = async () => {
     const response = await axios.get("https://mock-api-bdg1.onrender.com/data");
-
     setPosts(response.data);
   };
 
   const handleCreate = async (post) => {
     console.log("&&&&&&&&&&&& ", currentPost);
     if (currentPost) {
-
+      await axios.put(`https://mock-api-bdg1.onrender.com/data/${post.id}`, post);
     } else {
       await axios.post("https://mock-api-bdg1.onrender.com/data", post);
     }
-
     setCurrentPost(null);
     getPosts();
   };
 
   const handleCancel = () => {
+    console.log("Cancelling, currentPost before reset:", currentPost);
     setCurrentPost(null);
+    console.log("CurrentPost after reset attempt:", currentPost);
+  }
+
+  const handleEdit = (post) => {
+    setCurrentPost(post);
+  }
+
+  const handleDelete = async (id) => {
+    await axios.delete(`https://mock-api-bdg1.onrender.com/data/${id}`);
+    getPosts();
   }
 
   useEffect(() => {
@@ -36,15 +45,29 @@ function App() {
   return (
     <div className="App">
       <h1>Posts</h1>
-      <AppForm post={currentPost} handleSubmit={handleCreate} handleCancel={handleCancel} />
-      {posts.map(post => (
-        <div key={post.id}>
-          <h2>UserName: {post.username}</h2>
-          <h2>Email: {post.email}</h2>
-          <button onClick={() => { }}>Edit</button>
-          <button onClick={() => { }}>Delete</button>
-        </div>
-      ))}
+      <PostForm post={currentPost} handleSubmit={handleCreate} handleCancel={handleCancel} />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id}>
+              <td>{post.username}</td>
+              <td>{post.email}</td>
+              <td>
+                <button onClick={() => { handleEdit(post) }}>Edit</button>
+                <button onClick={() => { handleDelete(post.id) }}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   )
 }
--- CRUD-IMPLEMENTATION/src/PostForm.jsx
+++ CRUD-IMPLEMENTATION/src/PostForm.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
-function AppForm({ post: currentPost, handleSubmit, handleCancel }) {
+function PostForm({ post: currentPost, handleSubmit, handleCancel }) {
     const [post, setPost] = useState({ username: '', email: '' });
 
     useEffect(() => {
-        if (currentPost) setPost(currentPost);
+        if (!currentPost) {
+            setPost({ username: '', email: '' });
+        }
+        else {
+            setPost(currentPost);
+        };
     }, [currentPost]);
 
     const onChange = (e) => {
@@ -18,17 +23,20 @@ function AppForm({ post: currentPost, handleSubmit, handleCancel }) {
                 e.preventDefault();
                 handleSubmit(post);
             }}>
-                <label>UserName:</label>
+                <label>Name:</label>
                 <input type="text" name="username" value={post.username} onChange={onChange} required />
 
                 <label>Email:</label>
                 <input type='email' name="email" value={post.email} onChange={onChange} required />
 
                 <button type='submit'>Submit</button>
-                <button onClick={handleCancel}>Cancel</button>
+                <button type='button' onClick={(e) => {
+                    e.preventDefault();
+                    handleCancel();
+                }}>Cancel</button>
             </form>
-        </div>
+        </div >
     )
 }
 
-export default AppForm
+export default PostForm
